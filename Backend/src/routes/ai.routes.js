426de870f9b5c6af1bc312f3eacd7658f97f14d3
Shreya Chaurasia <144977937+shreyachaurasia0@users.noreply.github.com>
2025-08-06 07:22:19 +0530

    update

--- Backend/src/routes/ai.routes.js
+++ Backend/src/routes/ai.routes.js
@@ -3,6 +3,6 @@ const aiController = require('../controllers/ai.controller');
 
 const router = express.Router();
 
-router.post("/get-review" , aiController.getReview)
+router.post('/get-review', aiController.getReview);
 
 module.exports = router;
